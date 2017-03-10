afa376b2ae7f3e3885fa4a833a12ce370386bd96
Rubi Jihantoro <jihantoro2@gmail.com>
2017-03-10 14:16:32 +0700

    updating main js file
    updating main js file

--- blurbs.js
+++ blurbs.js
@@ -5,4 +5,413 @@
 *  kutemukan.com/profile/ruby						
 ******************************************************/
 
-function blurbs_readable_time(t,e,a,s,n,o){var i="human",r="",u="-",l=new Date,h=l.getFullYear(),b=l.getMonth(),c=l.getDate(),d=l.getHours(),f=l.getMinutes(),p=l.getSeconds(),y=e,g=a,m=s,v=n,_=o;"human"==i&&$("[blurbs-date]").each(function(){var t=$(this).attr("blurbs-date"),e=(t.split(/\-|\s/),new Date(t)),a=e.getFullYear(),s=e.getMonth(),n=e.getDate(),o=e.getHours(),i=e.getMinutes(),l=e.getSeconds(),w=n+1;if(h+r+b+r+c+r+d+r+f==a+r+s+r+n+r+o+r+i){var k=p-l;$("[blurbs-date='"+t+"'").html(k+y)}else if(h+r+b+r+c+r+d==a+r+s+r+n+r+o){var k=f-i;$("[blurbs-date='"+t+"'").html(k+g)}else if(h+r+b+r+c==a+r+s+r+n){var k=d-o;$("[blurbs-date='"+t+"'").html(k+m)}else if(h+r+b+r+c==a+r+s+r+w){var k=d-o;$("[blurbs-date='"+t+"'").html(v+o+":"+i)}else if(h+r+b==a+r+s){var k=c-n;$("[blurbs-date='"+t+"'").html(k+_+o+":"+i)}else h==a?$("[blurbs-date='"+t+"'").html(n+u+s+u+a):h>a&&$("[blurbs-date='"+t+"'").html(n+u+s+u+a)})}!function(){"use strict";var t=function(t,e){this.play("blurbs_couple",t,e)};t.prototype={constructor:t,play:function(t,e,a){this.type=t,this.$el=$(e),this.op=this.getOptions(a),this.enabled=!0,$("[couple-id]").each(function(){var t=$(this).attr("couple-id"),e=$("[couple-id='"+t+"'] > ul").attr("couple-data");$(this).on({click:function(){$("[couple-data='"+e+"']").addClass("visible").slideDown("slow")},mouseleave:function(){$($("[couple-data='"+e+"']")).on({mouseleave:function(){$("[couple-data='"+e+"']").removeClass("visible").slideUp("slow")}})}})})},getOptions:function(t){return t=$.extend({},$.fn[this.type].defaults,t,this.$el.data())}},$.fn.blurbs_couple=function(e){return this.each(function(){var a=$(this),s=a.data("blurbs_couple"),n="object"==typeof e&&e;s||a.data("blurbs_couple",s=new t(this,n)),"string"==typeof e&&s[e]()})},$.fn.blurbs_couple.Constructor=t,$.fn.blurbs_couple.defaults={type:"image",acton:"hover",identity:""}}(window.jQuery),!function(){"use strict";var t=function(t,e){this.play("blurbs_background",t,e)};t.prototype={constructor:t,play:function(t,e,a){this.type=t,this.$el=$(e),this.op=this.getOptions(a),$("[bg-id]").each(function(){var t=$(this).attr("bg-source"),e=$(this).attr("bg-repeat"),a=$(this).attr("bg-size");$(this).css({background:"url('"+t+"')","background-size":""+a,"background-repeat":""+e})})},getOptions:function(t){return t=$.extend({},$.fn[this.type].defaults,t,this.$el.data())}},$.fn.blurbs_background=function(e){return this.each(function(){var a=$(this),s=a.data("blurbs_background"),n="object"==typeof e&&e;s||a.data("blurbs_background",s=new t(this,n)),"string"==typeof e&&s[e]()})},$.fn.blurbs_background.Constructor=t,$.fn.blurbs_background.defaults={type:"image"}}(window.jQuery),!function(){"use strict";var t=function(t,e){this.play("blurbs_hotahomba",t,e)};t.prototype={constructor:t,play:function(t,e,a){this.type=t,this.$el=$(e),this.op=this.getOptions(a),$("[homba-id]").each(function(){var t=$(this).attr("homba-id"),e=$("[homba-id='"+t+"'");$("[homba-show='"+t+"'").text(e.val()),$(this).keyup(function(){$("[homba-show='"+t+"'").text(e.val()),$("[show]").each(function(){var t=$(this).attr("show"),e=($('[homba-id="'+t+'"]').attr("homba-id"),$("[show='"+t+"']")),a=t.replace(/!/i,""),s=$("input[homba-id='"+a+"']");0==t.search("!")&&!s.val()||t.search("!")<0&&s.val()?$(e).css({display:"block"}):(0==t.search("!")&&s.val()||t.search("!")<0&&!s.val())&&$(e).css({display:"none"})})}),$("[show]").each(function(){var t=$(this).attr("show"),e=($('[homba-id="'+t+'"]').attr("homba-id"),$("[show='"+t+"']")),a=t.replace(/!/i,""),s=$("input[homba-id='"+a+"']");0==t.search("!")&&!s.val()||t.search("!")<0&&s.val()?$(e).css({display:"block"}):(0==t.search("!")&&s.val()||t.search("!")<0&&!s.val())&&$(e).css({display:"none"})})})},getOptions:function(t){return t=$.extend({},$.fn[this.type].defaults,t,this.$el.data())}},$.fn.blurbs_hotahomba=function(e){return this.each(function(){var a=$(this),s=a.data("blurbs_hotahomba"),n="object"==typeof e&&e;s||a.data("blurbs_hotahomba",s=new t(this,n)),"string"==typeof e&&s[e]()})},$.fn.blurbs_hotahomba.Constructor=t,$.fn.blurbs_hotahomba.defaults={identity:""},$("[homba-id]").blurbs_hotahomba({identity:"snail"})}(window.jQuery),!function(){"use strict";var t=function(t,e){this.play("blurbs_date",t,e)};t.prototype={constructor:t,play:function(t,e,a){this.type=t,this.$el=$(e),this.op=this.getOptions(a);var t=this.op.type,s=new Date,n=(s.getFullYear(),s.getMonth(),s.getDate(),s.getHours(),s.getMinutes(),s.getSeconds(),this.op.ago),o=this.op.minute,i=this.op.hours,r=this.op.ytd,u=this.op.day,l=this.op.server;if(l){var h=new Date(l);blurbs_readable_time(h,n,o,i,r,u)}else{var h=new Date;blurbs_readable_time(h,n,o,i,r,u),setInterval(function(){blurbs_readable_time(h,n,o,i,r,u)},500)}},getOptions:function(t){return t=$.extend({},$.fn[this.type].defaults,t,this.$el.data())}},$.fn.blurbs_date=function(e){return this.each(function(){var a=$(this),s=a.data("blurbs_date"),n="object"==typeof e&&e;s||a.data("blurbs_date",s=new t(this,n)),"string"==typeof e&&s[e]()})},$.fn.blurbs_date.Constructor=t,$.fn.blurbs_date.defaults={ago:" Detik Yang Lalu",minute:"Menit Yang Lalu",hours:"Jam Yang Lalu",ytd:"Kemarin Pada Pukul",day:"Hari Yang Lalu",server:""}}(window.jQuery),!function(){"use strict";var t=function(t,e){this.play("blurbs_form",t,e)};t.prototype={constructor:t,play:function(t,e,a){this.type=t,this.$el=$(e),this.op=this.getOptions(a),$(".form > [type='submit']").each(function(){$(this).click(function(){$(".form > [required]").each(function(){var t=$(this).val();t?console.log("kucing"):$(this).hide()})})})},getOptions:function(t){return t=$.extend({},$.fn[this.type].defaults,t,this.$el.data())}},$.fn.blurbs_form=function(e){return this.each(function(){var a=$(this),s=a.data("blurbs_form"),n="object"==typeof e&&e;s||a.data("blurbs_form",s=new t(this,n)),"string"==typeof e&&s[e]()})},$.fn.blurbs_form.Constructor=t,$.fn.blurbs_form.defaults={identity:""},$("[homba-id]").blurbs_form({identity:"snail"})}(window.jQuery);
+/*****************************************************
+*  Blurbs Framework
+*  Writer : Rubi Jihantoro
+*
+*  #Couple
+******************************************************/
+
+!function () {
+
+  "use strict";
+
+  var Blurbs_couple = function (el, op) {
+  	this.play('blurbs_couple', el, op)
+  }
+
+  Blurbs_couple.prototype = 
+  {
+
+  	constructor: Blurbs_couple
+
+  	, play: function (type, el, op) {
+        this.type = type
+        this.$el = $(el)
+        this.op = this.getOptions(op)
+        this.enabled = true
+
+
+        $("[couple-id]").each(function (){
+          
+          var id     = $(this).attr("couple-id")
+          ,   source = $("[couple-id='"+id+"'] > ul").attr("couple-data");
+
+                    $(this).on({
+
+                        click: function(){
+                            $("[couple-data='"+source+"']").addClass('visible').slideDown('slow');
+                        }, 
+                        mouseleave: function(){
+
+                          $($("[couple-data='"+source+"']")).on({
+
+                              mouseleave: function(){
+                                
+                                $("[couple-data='"+source+"']").removeClass('visible').slideUp('slow');
+
+                              }
+
+                          });
+
+                        }
+
+                    });
+
+        });
+
+  	  }
+    , getOptions: function (op) {
+      op = $.extend({}, $.fn[this.type].defaults, op, this.$el.data())
+
+      return op
+    }
+
+  }
+
+$.fn.blurbs_couple = function ( option ) {
+    return this.each(function () {
+      var $this = $(this)
+        , data = $this.data('blurbs_couple')
+        , op = typeof option == 'object' && option
+
+      if (!data) $this.data('blurbs_couple', (data = new Blurbs_couple(this, op)))
+      if (typeof option == 'string') data[option]()
+    })
+}
+
+  $.fn.blurbs_couple.Constructor = Blurbs_couple
+
+ $.fn.blurbs_couple.defaults = {
+    type: "image",
+    acton : "hover",
+    identity: ""
+  }
+
+
+}(window.jQuery);
+
+/*****************************************************
+*  Blurbs Framework
+*  Writer : Rubi Jihantoro
+*
+*  #Background
+******************************************************/
+
+!function () {
+
+  "use strict";
+
+  var Blurbs_background = function (el, op) {
+    this.play('blurbs_background', el, op)
+  }
+
+  Blurbs_background.prototype = 
+  {
+
+    constructor: Blurbs_background
+
+    , play: function (type, el, op) {
+        this.type = type
+        this.$el = $(el)
+        this.op = this.getOptions(op)
+
+        $("[bg-id]").each(function (){
+          
+          var source = $(this).attr("bg-source")
+          ,   repeat = $(this).attr("bg-repeat")
+          ,   size   = $(this).attr("bg-size");
+          $(this).css({"background": "url('"+source+"')", "background-size": ""+ size +"", "background-repeat": ""+repeat+""});
+
+        });
+
+      }
+    , getOptions: function (op) {
+      op = $.extend({}, $.fn[this.type].defaults, op, this.$el.data())
+
+      return op
+    }
+
+  }
+
+$.fn.blurbs_background = function ( option ) {
+    return this.each(function () {
+      var $this = $(this)
+        , data = $this.data('blurbs_background')
+        , op = typeof option == 'object' && option
+
+      if (!data) $this.data('blurbs_background', (data = new Blurbs_background(this, op)))
+      if (typeof option == 'string') data[option]()
+
+    })
+}
+
+  $.fn.blurbs_background.Constructor = Blurbs_background
+
+ $.fn.blurbs_background.defaults = {
+    type : "image"
+  }
+
+
+}(window.jQuery);
+
+/*****************************************************
+*  Blurbs Framework
+*  Writer : Rubi Jihantoro
+*
+*  #Hotahomba
+******************************************************/
+
+!function () {
+
+  "use strict";
+
+  var Blurbs_hotahomba = function (el, op) {
+    this.play('blurbs_hotahomba', el, op)
+  }
+
+  Blurbs_hotahomba.prototype = 
+  {
+
+    constructor: Blurbs_hotahomba
+
+    , play: function (type, el, op) {
+        this.type = type
+        this.$el = $(el)
+        this.op = this.getOptions(op)
+
+        $("[homba-id]").each(function (){
+          
+          var source = $(this).attr("homba-id")
+          ,   ayam   = $("[homba-id='"+source+"'")
+
+          $("[homba-show='"+source+"'").text(ayam.val());
+
+          $(this).keyup(function () {
+            $("[homba-show='"+source+"'").text(ayam.val());
+            $("[show]").each(function (){
+              
+              var src  = $(this).attr("show")
+              , srct      = $('[homba-id="'+src+'"]').attr("homba-id")
+              , srcs      = $("[show='"+src+"']")
+              , pid       = src.replace(/!/i,'')
+              , srcw      = $("input[homba-id='"+pid+"']");
+              
+                if(src.search('!') == 0 && !srcw.val() || src.search('!') < 0 && srcw.val()) //mau tampil pas kosong
+              {
+                $(srcs).css({"display": "block"});
+              }
+                else if(src.search('!') == 0 && srcw.val() || src.search('!') < 0 && !srcw.val()) //mau tampil pas kosong tapi ke  isi
+              {
+                $(srcs).css({"display": "none"});
+              }
+
+            });
+          });
+
+          $("[show]").each(function (){
+              
+              var src  = $(this).attr("show")
+              , srct      = $('[homba-id="'+src+'"]').attr("homba-id")
+              , srcs      = $("[show='"+src+"']")
+              , pid       = src.replace(/!/i,'')
+              , srcw      = $("input[homba-id='"+pid+"']");
+              
+                if(src.search('!') == 0 && !srcw.val() || src.search('!') < 0 && srcw.val()) //mau tampil pas kosong
+              {
+                $(srcs).css({"display": "block"});
+              }
+                else if(src.search('!') == 0 && srcw.val() || src.search('!') < 0 && !srcw.val()) //mau tampil pas kosong tapi ke  isi
+              {
+                $(srcs).css({"display": "none"});
+              }
+
+            });
+
+          });
+
+      }
+    , getOptions: function (op) {
+      op = $.extend({}, $.fn[this.type].defaults, op, this.$el.data())
+
+      return op
+    }
+
+  }
+
+$.fn.blurbs_hotahomba = function ( option ) {
+    return this.each(function () {
+      var $this = $(this)
+        , data = $this.data('blurbs_hotahomba')
+        , op = typeof option == 'object' && option
+
+      if (!data) $this.data('blurbs_hotahomba', (data = new Blurbs_hotahomba(this, op)))
+      if (typeof option == 'string') data[option]()
+
+    })
+}
+
+  $.fn.blurbs_hotahomba.Constructor = Blurbs_hotahomba
+
+ $.fn.blurbs_hotahomba.defaults = {
+    identity: ""
+  }
+
+  $('[homba-id]').blurbs_hotahomba({
+      identity : "snail"
+  });
+
+}(window.jQuery);
+
+/*****************************************************
+*  Blurbs Framework
+*  Writer : Rubi Jihantoro
+*
+*  #Date
+******************************************************/
+
+!function () {
+
+  "use strict";
+
+  var Blurbs_date = function (el, op) {
+    this.play('blurbs_date', el, op)
+  }
+
+  Blurbs_date.prototype = 
+  {
+
+    constructor: Blurbs_date
+
+    , play: function (type, el, op) {
+        this.type = type
+        this.$el = $(el)
+        this.op = this.getOptions(op)
+
+        var type    = this.op.type
+        , s       = ""
+        , st      = "-"
+        , now     = new Date()
+        , y       = now.getFullYear()
+        , m       = now.getMonth()
+        , d       = now.getDate()
+        , h       = now.getHours()
+        , i       = now.getMinutes()
+        , a       = now.getSeconds()
+        , ago     = this.op.ago
+        , minute  = this.op.minute
+        , hours   = this.op.hours
+        , ystd    = this.op.ytd
+        , day     = this.op.day
+        , server  = this.op.server;
+
+          if(!server)
+        {
+          var ser = new Date()
+          blurbs_readable_time(ser,ago,minute,hours,ystd,day)
+          setInterval(function(){blurbs_readable_time(ser,ago,minute,hours,ystd,day)},500);
+        }
+          else
+        {
+          var ser = new Date(server)
+          blurbs_readable_time(ser,ago,minute,hours,ystd,day)
+        }
+      }
+    , getOptions: function (op) 
+      {
+        op = $.extend({}, $.fn[this.type].defaults, op, this.$el.data())
+
+        return op
+      }
+  }
+
+$.fn.blurbs_date = function ( option ) {
+    return this.each(function () {
+      var $this = $(this)
+        , data = $this.data('blurbs_date')
+        , op = typeof option == 'object' && option
+
+      if (!data) $this.data('blurbs_date', (data = new Blurbs_date(this, op)))
+      if (typeof option == 'string') data[option]()
+
+    })
+}
+  
+$.fn.blurbs_date.Constructor = Blurbs_date
+
+ $.fn.blurbs_date.defaults = {
+    ago: ' Detik Yang Lalu',
+    minute: 'Menit Yang Lalu',
+    hours: 'Jam Yang Lalu',
+    ytd: 'Kemarin Pada Pukul',
+    day: 'Hari Yang Lalu',
+    server: ''
+  }
+
+}(window.jQuery);
+
+function blurbs_readable_time(sd,lg,lm,lh,ly,ld)
+{
+        var type    = 'human'
+        , s       = ""
+        , st      = "-"
+        , now     = new Date()
+        , y       = now.getFullYear()
+        , m       = now.getMonth()
+        , d       = now.getDate()
+        , h       = now.getHours()
+        , i       = now.getMinutes()
+        , a       = now.getSeconds()
+        , ago     = lg
+        , minute  = lm
+        , hours   = lh
+        , ystd    = ly
+        , day     = ld;
+
+          if(type == "human")
+        {
+          $("[blurbs-date]").each(function (){
+            var ew = $(this).attr("blurbs-date")
+            , split    = ew.split(/\-|\s/)
+            , dnow     = new Date(ew)
+            , dy       = dnow.getFullYear()
+            , dm       = dnow.getMonth()
+            , dd       = dnow.getDate()
+            , dh       = dnow.getHours()
+            , di       = dnow.getMinutes()
+            , da       = dnow.getSeconds()
+            , ytd     = dd + 1;
+
+            if(y+s+m+s+d+s+h+s+i == dy+s+dm+s+dd+s+dh+s+di)
+            {
+              var o = a-da;
+              $("[blurbs-date='"+ew+"'").html(o + ago)
+            }
+            else if(y+s+m+s+d+s+h == dy+s+dm+s+dd+s+dh)
+            {
+              var o = i-di;
+              $("[blurbs-date='"+ew+"'").html(o+minute)
+            }
+            else if(y+s+m+s+d == dy+s+dm+s+dd)
+            {
+              var o = h-dh;
+              $("[blurbs-date='"+ew+"'").html(o+hours)
+            }
+            else if(y+s+m+s+d == dy+s+dm+s+ytd)
+            {
+              var o = h-dh;
+              $("[blurbs-date='"+ew+"'").html(ystd+dh+":"+di)
+            }
+            else if(y+s+m == dy+s+dm)
+            {
+              var o = d-dd;
+              $("[blurbs-date='"+ew+"'").html(o+day+dh+":"+di)
+            }
+            else if(y == dy)
+              $("[blurbs-date='"+ew+"'").html(dd+st+dm+st+dy)
+            else if(y > dy)
+              $("[blurbs-date='"+ew+"'").html(dd+st+dm+st+dy)
+          });
+        }
+}
+
